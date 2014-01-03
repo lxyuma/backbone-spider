529c2a80de22684aff0375aa8c08172a80b7b5bd
ryuma.tsukano <lyumalyuma@gmail.com>
2014-01-04 00:49:24 +0900

    add spyAllFunc

--- backbone-spider.js
+++ backbone-spider.js
@@ -6,8 +6,42 @@
 (function(Backbone) {
 
   _.extend(Backbone.View.prototype, {
+
     spyAllFunc: function() {
+      this.allFunc = this._getAllFunc();
+      _.map(this.allFunc, function(prop){
+        sinon.spy(this, prop);
+      }, this);
+    },
+    restoreAllFunc: function() {
+      _.map(this.allFunc, function(prop){
+        this[prop].restore();
+      }, this);
+    },
+    _getAllFunc: function(){
+      var allFunc = [];
+      for (var prop in this){
+        if (typeof this[prop] === "function") {
+          allFunc.push(prop);
+        };
+      };
+      return allFunc;
     }
+
+
+    // namespace
+    //spider: {
+    //  _setFuncOnce: function(){
+    //    if(typeof this.allProp !== "object") {
+    //      for (var prop in this){
+    //        this.allProp.push(prop);
+    //        if (this.hasOwnProperty(prop)){
+    //          this.originalFunc.push(prop);
+    //        }
+    //      };
+    //    };
+    //  };
+    //}
   });
 
 }).call(this, Backbone, sinon);
--- test/spec/backbone-spider.js
+++ test/spec/backbone-spider.js
@@ -1,11 +1,33 @@
 /*global describe, it */
 'use strict';
 (function () {
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
-
-            });
-        });
+  describe('Backbone Spider', function () {
+    before(function(){
+      this.MyView = Backbone.View.extend({
+        url: "faked",
+        originalMethod: function(){
+          console.log('have called original method in test');
+        }
+      });
     });
+    beforeEach(function(){
+      this.view = new this.MyView();
+    });
+    describe('spyAllFunc()', function () {
+      beforeEach(function(){
+        this.view.spyAllFunc();
+      });
+      afterEach(function(){
+        this.view.restoreAllFunc();
+      });
+      it('should spy Backbone.View function', function () {
+        this.view.render();
+        expect(this.view.render.calledOnce).to.be.true;
+      });
+      it('should spy YourView function', function () {
+        this.view.originalMethod();
+        expect(this.view.originalMethod.calledOnce).to.be.true;
+      });
+    });
+  });
 })();
