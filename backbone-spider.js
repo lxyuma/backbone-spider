2d163e4c5de3a3e8ba1e27b02bfa0310014702de
ryuma.tsukano <lyumalyuma@gmail.com>
2014-01-04 15:20:37 +0900

    mod tab width

--- backbone-spider.js
+++ backbone-spider.js
@@ -5,32 +5,32 @@
 
 (function(Backbone) {
 
-  _.extend(Backbone.View.prototype, {
+    _.extend(Backbone.View.prototype, {
 
-    spyAllFunc: function() {
-      this.allFunc = this._getAllFunc();
-      _.map(this.allFunc, function(prop){
-        sinon.spy(this, prop);
-      }, this);
-    },
-    restoreAllFunc: function() {
-      _.map(this.allFunc, function(prop){
-        if (this._isSpy(this[prop])){
-          this[prop].restore();
-        };
-      }, this);
-    },
-    _getAllFunc: function(){
-      var allFunc = [];
-      for (var prop in this){
-        if (typeof this[prop] === "function") {
-          allFunc.push(prop);
-        };
-      };
-      return allFunc;
-    },
-    _isSpy: function(func){
-      return func.hasOwnProperty('called');
-    }
-  });
+        spyAllFunc: function() {
+            this.allFunc = this._getAllFunc();
+            _.map(this.allFunc, function(prop){
+                sinon.spy(this, prop);
+            }, this);
+        },
+        restoreAllFunc: function() {
+            _.map(this.allFunc, function(prop){
+                if (this._isSpy(this[prop])){
+                    this[prop].restore();
+                };
+            }, this);
+        },
+        _getAllFunc: function(){
+            var allFunc = [];
+            for (var prop in this){
+                if (typeof this[prop] === "function") {
+                    allFunc.push(prop);
+                };
+            };
+            return allFunc;
+        },
+        _isSpy: function(func){
+            return func.hasOwnProperty('called');
+        }
+    });
 }).call(this, Backbone, sinon);
--- test/spec/backbone-spider.js
+++ test/spec/backbone-spider.js
@@ -1,52 +1,52 @@
 /*global describe, it */
 'use strict';
 (function () {
-  describe('Backbone Spider', function () {
-    before(function(){
-      this.MyView = Backbone.View.extend({
-        url: "faked",
-        originalMethod: function(){
-          console.log('have called original method in test');
-        }
-      });
-    });
-    beforeEach(function(){
-      this.view = new this.MyView();
-    });
-    describe('spyAllFunc()', function () {
-      describe('basic usage', function(){
+    describe('Backbone Spider', function () {
+        before(function(){
+            this.MyView = Backbone.View.extend({
+                url: "faked",
+                originalMethod: function(){
+                    console.log('have called original method in test');
+                }
+            });
+        });
         beforeEach(function(){
-          this.view.spyAllFunc();
+            this.view = new this.MyView();
         });
-        afterEach(function(){
-          this.view.restoreAllFunc();
+        describe('spyAllFunc()', function () {
+            describe('basic usage', function(){
+                beforeEach(function(){
+                    this.view.spyAllFunc();
+                });
+                afterEach(function(){
+                    this.view.restoreAllFunc();
+                });
+                it('should spy Backbone.View function', function () {
+                    this.view.render();
+                    expect(this.view.render.calledOnce).to.be.true;
+                });
+                it('should spy YourView function', function () {
+                    this.view.originalMethod();
+                    expect(this.view.originalMethod.calledOnce).to.be.true;
+                });
+                it('should not affect to sinon.stub (if you use restore)', function(){
+                    this.view.render.restore();
+                    sinon.stub(this.view, "render").returns('fakedTrue');
+                    expect(this.view.render()).to.eql('fakedTrue');
+                    this.view.render.restore();
+                });
+            });
         });
-        it('should spy Backbone.View function', function () {
-          this.view.render();
-          expect(this.view.render.calledOnce).to.be.true;
+        describe('restoreAllFunc()', function(){
+            beforeEach(function(){
+                this.view.spyAllFunc();
+            });
+            it('should restore sinon wrapper', function(){
+                this.view.render();
+                expect(this.view.render.hasOwnProperty('called')).to.be.true;
+                this.view.restoreAllFunc();
+                expect(this.view.render.hasOwnProperty('called')).to.be.false;
+            });
         });
-        it('should spy YourView function', function () {
-          this.view.originalMethod();
-          expect(this.view.originalMethod.calledOnce).to.be.true;
-        });
-        it('should not affect to sinon.stub (if you use restore)', function(){
-          this.view.render.restore();
-          sinon.stub(this.view, "render").returns('fakedTrue');
-          expect(this.view.render()).to.eql('fakedTrue');
-          this.view.render.restore();
-        });
-      });
     });
-    describe('restoreAllFunc()', function(){
-      beforeEach(function(){
-        this.view.spyAllFunc();
-      });
-      it('should restore sinon wrapper', function(){
-        this.view.render();
-        expect(this.view.render.hasOwnProperty('called')).to.be.true;
-        this.view.restoreAllFunc();
-        expect(this.view.render.hasOwnProperty('called')).to.be.false;
-      });
-    });
-  });
 })();
